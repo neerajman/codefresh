80cdfa471cc96a18ba28d2ea3ae466e7a15d461a
neerajman <neeraj.dan@outlook.com>
2021-03-14 11:10:29 +0530

    added region and zone
    zone and region added

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project = "My Project"
-  region  = "europe-west2"
-  zone    = "europe-west2-b"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
 resource "google_compute_instance" "vm_instance" {
